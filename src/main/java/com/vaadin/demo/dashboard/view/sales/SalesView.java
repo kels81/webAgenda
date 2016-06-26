2c378bf0726d1a4552bada2b2cf4f7ae5f82e4e2
Edrd <Edrd@Eduardo>
2016-06-26 17:06:17 -0500

    mejoras

--- src/main/java/com/vaadin/demo/dashboard/view/sales/SalesView.java
+++ src/main/java/com/vaadin/demo/dashboard/view/sales/SalesView.java
@@ -1,34 +1,10 @@
 package com.vaadin.demo.dashboard.view.sales;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-
-import org.vaadin.maddon.ListContainer;
-
-import com.vaadin.addon.charts.model.style.SolidColor;
-import com.vaadin.addon.timeline.Timeline;
-import com.vaadin.data.Container;
-import com.vaadin.data.Property.ValueChangeEvent;
-import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.demo.dashboard.DashboardUI;
-import com.vaadin.demo.dashboard.domain.Movie;
-import com.vaadin.demo.dashboard.domain.MovieRevenue;
-import com.vaadin.event.ShortcutAction.KeyCode;
-import com.vaadin.event.ShortcutListener;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.Responsive;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.ComboBox;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -39,36 +15,34 @@ import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings("serial")
 public class SalesView extends VerticalLayout implements View {
-    
-    private FormLayout details;
+
+    private final HorizontalLayout details;
+    //private final FormLayout details;
     private TextField txtNombre;
     private TextField txtApPaterno;
     private TextField txtApmaterno;
     private OptionGroup rdbGenero;
     private TextField txtEmail;
     private TextField txtTelefono;
-    private TextField emailField;
 
     public SalesView() {
-        setSizeFull();
-        addStyleName("sales");
+        //setSizeFull();
+        //addStyleName("sales");
+        addStyleName("profile-window");     //importante
+        Responsive.makeResponsive(this);    //importante
+        //setMargin(true);
 
         addComponent(buildHeader());
 
         details = buildForm();
         addComponent(details);
         setExpandRatio(details, 1);
-        /*Label h1 = new Label("Text Fields");
-         h1.setSizeUndefined();
-         h1.addStyleName(ValoTheme.LABEL_H1);
-         h1.addStyleName(ValoTheme.LABEL_NO_MARGIN);
-         addComponent(h1);*/
+
     }
 
     private Component buildHeader() {
-        HorizontalLayout header = new HorizontalLayout();
+        VerticalLayout header = new VerticalLayout();
         header.addStyleName("viewheader");
-        header.setSpacing(true);
         Responsive.makeResponsive(header);
 
         Label titleLabel = new Label("Revenue by Movie");
@@ -78,31 +52,63 @@ public class SalesView extends VerticalLayout implements View {
 
         header.addComponents(titleLabel);
 
-        /*TextField tf = new TextField("Normal");
-         tf.setInputPrompt("First name");
-         header.addComponent(tf);
-
-         tf = new TextField("Custom color");
-         tf.setInputPrompt("Email");
-         tf.addStyleName("color1");
-         header.addComponent(tf);*/
         return header;
     }
 
-    private FormLayout buildForm() {
-        FormLayout form = new FormLayout();
-        form.addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
-        
+    private HorizontalLayout buildForm() {
+        //private FormLayout buildForm() {
+        HorizontalLayout detailsForm = new HorizontalLayout();
+        detailsForm.setWidth(100.0f, Unit.PERCENTAGE);                      //importante
+        detailsForm.setMargin(new MarginInfo(false, true, true, true));
+        detailsForm.setSpacing(true);
+
+        FormLayout form1 = new FormLayout();
+        form1.addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
+        //form.setWidth(100.0f, Unit.PERCENTAGE);                      //importante
+        FormLayout form2 = new FormLayout();
+        form2.addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
+        //form.setWidth(100.0f, Unit.PERCENTAGE);                      //importante
+        detailsForm.addComponent(form1);
+        detailsForm.addComponent(form2);
+
+        Label lblSeccion = new Label("Datos Generales");
+        lblSeccion.addStyleName(ValoTheme.LABEL_H4);
+        lblSeccion.addStyleName(ValoTheme.LABEL_COLORED);
+        form1.addComponent(lblSeccion);
+
         TextField tf = new TextField("Normal");
         tf.setInputPrompt("First name");
-        form.addComponent(tf);
+        form1.addComponent(tf);
 
-        tf = new TextField("Custom color");
+        tf = new TextField("Color");
         tf.setInputPrompt("Email");
         tf.addStyleName("color1");
-        form.addComponent(tf);
+        form1.addComponent(tf);
 
-        return form;
+        Label label = new Label("");
+        //label.setSizeUndefined();
+        form1.addComponent(label);
+
+        Label lblSeccion2 = new Label("Datos Generales");
+        lblSeccion2.addStyleName(ValoTheme.LABEL_H4);
+        lblSeccion2.addStyleName(ValoTheme.LABEL_COLORED);
+        form2.addComponent(lblSeccion2);
+
+        TextField tf2 = new TextField("Normal");
+        tf2.setInputPrompt("First name");
+        form2.addComponent(tf2);
+
+        tf2 = new TextField("Custom");
+        tf2.setInputPrompt("Email");
+        tf2.addStyleName("color1");
+        form2.addComponent(tf2);
+              
+        Label label2 = new Label("");
+        //label.setSizeUndefined();
+        form2.addComponent(label2);
+
+        //return form;
+        return detailsForm;
 
     }
 
