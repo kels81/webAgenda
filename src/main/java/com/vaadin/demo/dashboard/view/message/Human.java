31d20728314bc11e34a5e7ecca7ba7ca8c02a933
Edrd <Edrd@Eduardo>
2016-08-09 23:12:11 -0500

    mejoras

---
+++ src/main/java/com/vaadin/demo/dashboard/view/message/Human.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vaadin.demo.dashboard.view.message;
+
+import com.vaadin.server.FontIcon;
+import com.vaadin.server.GenericFontIcon;
+
+/**
+ *
+ * @author Edrd
+ */
+public enum Human implements FontIcon {
+    USER_36(0XF137);
+    
+    private final int codepoint;
+    public static final String FONT_FAMILY = "Human";
+
+    Human(int codepoint) {
+        this.codepoint = codepoint;
+    }
+
+    @Override
+    public String getFontFamily() {
+        return FONT_FAMILY;
+    }
+
+    @Override
+    public int getCodepoint() {
+        return codepoint;
+    }
+
+    @Override
+    public String getHtml() {
+        return GenericFontIcon.getHtml(Human.FONT_FAMILY, codepoint);
+    }
+
+    @Override
+    public String getMIMEType() {
+        throw new UnsupportedOperationException(
+                FontIcon.class.getSimpleName()
+                + " should not be used where a MIME type is needed.");
+    }
+
+}
