c4d151b26ee14c7afe96e406cf64b97f740f441b
Edrd <Edrd@Eduardo>
2016-08-09 23:12:30 -0500

    mejoras

---
+++ src/main/java/com/vaadin/demo/dashboard/view/message/Ui_Kit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vaadin.demo.dashboard.view.message;
+
+import com.vaadin.server.FontIcon;
+import com.vaadin.server.GenericFontIcon;
+
+/**
+ *
+ * @author Edrd
+ */
+public enum Ui_Kit implements FontIcon {
+    USER_1(0XF159);
+    
+    private final int codepoint;
+    public static final String FONT_FAMILY = "Ui_Kit";
+
+    Ui_Kit(int codepoint) {
+        this.codepoint = codepoint;
+    }
+
+    @Override
+    public String getFontFamily() {
+        return FONT_FAMILY;
+    }
+
+    @Override
+    public int getCodepoint() {
+        return codepoint;
+    }
+
+    @Override
+    public String getHtml() {
+        return GenericFontIcon.getHtml(Ui_Kit.FONT_FAMILY, codepoint);
+    }
+
+    @Override
+    public String getMIMEType() {
+        throw new UnsupportedOperationException(
+                FontIcon.class.getSimpleName()
+                + " should not be used where a MIME type is needed.");
+    }
+
+}
